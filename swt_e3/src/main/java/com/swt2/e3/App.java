87bae0d6ded99d5a805eb20bcfc2b4f6ed6c96ab
mayamahroum <58133999+mayamahroum@users.noreply.github.com>
2023-11-16 12:22:31 +0100

    add App.java

---
+++ swt_e3/src/main/java/com/swt2/e3/App.java
@@ -0,0 +1,13 @@
+package com.swt2.e3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
